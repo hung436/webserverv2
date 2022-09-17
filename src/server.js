3bdd6612195095800e5841994ceb3f807d201b91
hung436 <ttldhung@gmail.com>
2022-09-17 14:55:55 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+// import configViewEngine from './config/viewEngine';
+// import initWebRoute from './route/web';
+// import connectDB from './config/connectDB.JS';
+import bodyParser from 'body-parser';
+
+// import cors from 'cors';
+const app = express();
+require('dotenv').config();
+// app.use(cors());
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+
+  // Request headers you wish to allow
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+const port = process.env.PORT || 8080;
+// connectDB();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// initWebRoute(app);
+// configViewEngine(app);
+app.listen(port, () => {
+  console.log(`Server app listening on port ${port}`);
+});
+module.exports = app;
