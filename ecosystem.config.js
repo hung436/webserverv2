1c5e08eb62ee21e6095e288f48f96a1d606b4ab3
hung436 <ttldhung@gmail.com>
2022-10-05 00:19:57 +0700

    ok

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,23 @@
 module.exports = {
   apps: [
     {
-      script: "--exec ./node_modules/.bin/babel-node ./src/server.js",
+      name: "start",
+      script: "./node_modules/.bin/babel-node" && "./src/server.js",
       watch: ".",
-    },
-    {
-      script: "./service-worker/",
-      watch: ["./service-worker"],
     },
   ],
 
-  deploy: {
-    production: {
-      user: "SSH_USERNAME",
-      host: "SSH_HOSTMACHINE",
-      ref: "origin/master",
-      repo: "GIT_REPOSITORY",
-      path: "DESTINATION_PATH",
-      "pre-deploy-local": "",
-      "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
-    },
-  },
+  // deploy: {
+  //   production: {
+  //     user: "SSH_USERNAME",
+  //     host: "SSH_HOSTMACHINE",
+  //     ref: "origin/master",
+  //     repo: "GIT_REPOSITORY",
+  //     path: "DESTINATION_PATH",
+  //     "pre-deploy-local": "",
+  //     "post-deploy":
+  //       "npm install && pm2 reload ecosystem.config.js --env production",
+  //     "pre-setup": "",
+  //   },
+  // },
 };
